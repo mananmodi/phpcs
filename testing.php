bfb6a9c13588adefc7d5d9a4631acddeb47385cf
mananmodi <modi.manan14@gmail.com>
2024-02-12 15:36:34 +0530

    Update testing.php

--- testing.php
+++ testing.php
@@ -4,7 +4,13 @@
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p  >Hello World</p>'; 
+    <?php 
+        function CAMELCASETET(){
+            return "hello world";
+        }
+
+        CAMELCASETET();    
+         echo '<p  >Hello World</p>'; 
         die();
         ?>
     </body>
